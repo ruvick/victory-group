7ebc5ec116aded75c20d6be745f9064f3b509a1a
Ruvick <rudikov.web@ya.ru>
2024-09-24 15:06:37 +0300

    Калькулятор Ваша выгода

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -10,12 +10,28 @@ toggles.forEach(toggle => {
 	toggle.addEventListener('change', function() {
 		// Находим родительский элемент с классом item-actions-product
 		const parentItem = this.closest('.item-actions-product');
-		
+
 		// Проверяем состояние переключателя и добавляем/удаляем класс _active
-		if (this.checked) {
-			parentItem.classList.add('_active');
-		} else {
-			parentItem.classList.remove('_active');
-		}
+		parentItem.classList.toggle('_active', this.checked);
 	});
+});
+//========================================================================================================================================================
+
+const toggleInputs = document.querySelectorAll('.switch-block__input');
+const benefitPriceElement = document.querySelector('.benefit-product__price');
+
+// Функция для обновления цены выгоды
+function updateBenefitPrice() {
+  const totalDiscount = [...toggleInputs].filter(toggle => toggle.checked).reduce((acc, toggle) => {
+    const itemActionsProduct = toggle.closest('.item-actions-product');
+    const priceText = itemActionsProduct.querySelector('.item-actions-product__price').textContent;
+    const priceValue = parseInt(priceText.replace(/[^0-9]/g, ''), 10);
+    return acc + priceValue;
+  }, 0);
+
+  // Форматируем число с пробелами
+  benefitPriceElement.textContent = `${totalDiscount.toLocaleString('ru-RU')} ₽`;
+}
+toggleInputs.forEach(toggle => {
+	toggle.addEventListener('change', updateBenefitPrice);
 });
