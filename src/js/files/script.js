cdc89c422fb381da80b6f0481fcc0f3827cd1c9b
Ruvick <rudikov.web@ya.ru>
2024-09-24 10:18:48 +0300

    Функционал toggle

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,3 +1,21 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
-// import { isMobile } from "./functions.js";
+import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
+
+// Активация Toggle
+const toggles = document.querySelectorAll('.switch-block__input');
+
+// Добавляем обработчик события для каждого переключателя
+toggles.forEach(toggle => {
+	toggle.addEventListener('change', function() {
+		// Находим родительский элемент с классом item-actions-product
+		const parentItem = this.closest('.item-actions-product');
+		
+		// Проверяем состояние переключателя и добавляем/удаляем класс _active
+		if (this.checked) {
+			parentItem.classList.add('_active');
+		} else {
+			parentItem.classList.remove('_active');
+		}
+	});
+});
